7151fc0ca2a7407d5881787f89d92aa3e9e6c097
mohal28 <mohammadali.vip@gmail.com>
2021-07-05 00:28:29 +0800

    project setup

---
+++ src/main/java/com/mohal/springkafkatemplate/SpringKafkaTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.mohal.springkafkatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohal/springkafkatemplate/SpringKafkaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohal.springkafkatemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
